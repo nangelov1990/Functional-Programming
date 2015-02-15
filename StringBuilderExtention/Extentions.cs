c3ea4fffb235fedbdb51a1ad36e329a37c717075
Nick Angelov <nangelov1990@gmail.com>
2015-02-15 23:51:17 +0200

    First commit

---
+++ StringBuilderExtention/Extentions.cs
@@ -0,0 +1,42 @@
+﻿namespace StringBuilderExtention
+{
+    using System;
+    using System.Text;
+
+    public static class Extentions
+    {
+        public static string Substring(this StringBuilder stringBuilderElement, int startIndex, int length)
+        {
+            var maxLength = stringBuilderElement.Length;
+            Func<int, int, bool> outOfRange =
+                (firstIndex, lengthOfArgument) =>
+                    firstIndex > maxLength || (firstIndex + lengthOfArgument) > maxLength;
+
+            if (outOfRange(startIndex, length))
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            var substringValue = stringBuilderElement.ToString().Substring(startIndex, length);
+
+            return substringValue;
+        }
+
+        public static StringBuilder RemoveText(this StringBuilder stringBuilderElement, string text)
+        {
+            stringBuilderElement.Replace(text, string.Empty);
+
+            return stringBuilderElement;
+        }
+
+        private static void Main()
+        {
+            var strBuilder = new StringBuilder("sdkfhsdjnkfsdnfjsdkfnsdjghsdngjsdgnsg");
+            var substring = strBuilder.Substring(5, 7);
+            Console.WriteLine(substring);
+
+            var removeText = strBuilder.RemoveText("sdk");
+            Console.WriteLine(removeText);
+        }
+    }
+}
